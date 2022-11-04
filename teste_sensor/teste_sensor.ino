e2da1a19485c24e794213d617727f00e8dc0411f
Lucas Mateus <lmateus1067@outlook.com>
2022-11-04 10:10:33 -0300

    add code

---
+++ teste_sensor/teste_sensor.ino
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <DHT.h>
+#include <ThingSpeak.h>
+ 
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+WiFiClient client;
+DHT dht(DHTPIN, DHTTYPE); // Sensor that reads the temperature and humidity
+unsigned long myChannelNumber = 1;
+const char * myWriteAPIKey = "IJR99VIKPE8KS1Y8";
+
+
+//conect wifi
+const char* ssid = "UFRN_CERES"; //Enter your WIFI ssid
+const char* password = "UFRN@1306"; //Enter your WIFI password
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 360000;
+
+// Variable to hold temperature readings
+float temperatureC;
+float humidity;
+float sensorLdr;
+
+
+void setup() {
+  dht.begin();
+  Serial.begin(115200);  //Initialize serial
+  WiFi.mode(WIFI_STA);   
+  
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    
+    // Connect or reconnect to WiFi
+    if(WiFi.status() != WL_CONNECTED){
+      Serial.print("Attempting to connect");
+      while(WiFi.status() != WL_CONNECTED){
+        WiFi.begin(ssid, password); 
+        delay(5000);     
+      } 
+      Serial.println("\nConnected.");
+    }
+
+    // Get a new temperature reading
+    temperatureC = dht.readTemperature();
+    Serial.print("Temperature (ºC): ");
+    Serial.println(temperatureC);
+    humidity = dht.readHumidity();
+    Serial.print("Humidity (%): ");
+    Serial.println(humidity);
+
+    sensorLdr = analogRead(A0);
+    Serial.print("Luminous (%): ");
+    Serial.println(sensorLdr);
+    
+    //uncomment if you want to get temperature in Fahrenheit
+    /*temperatureF = 1.8 * bme.readTemperature() + 32;
+    Serial.print("Temperature (ºC): ");
+    Serial.println(temperatureF);*/
+
+    // set the fields with the values
+    ThingSpeak.setField(1, temperatureC);
+    ThingSpeak.setField(2, humidity);
+    ThingSpeak.setField(3, sensorLdr);
+    
+    
+    // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
+    // pieces of information in a channel.  Here, we write to field 1.
+    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+    //uncomment if you want to get temperature in Fahrenheit
+    //int x = ThingSpeak.writeField(myChannelNumber, 1, temperatureF, myWriteAPIKey);
+
+    if(x == 200){
+      Serial.println("Channel update successful.");
+    }
+    else{
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+    }
+    lastTime = millis();
+  }
+}
